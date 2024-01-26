833fb64118c88c72a82a49abea87c807312b1b3e
Neel Sadafule <neel.sadafule13@gmail.com>
2024-01-25 22:21:24 -0800

    Fixed DQarraytest

--- Assignment 1/DQarraytest.cpp
+++ Assignment 1/DQarraytest.cpp
@@ -5,16 +5,22 @@ int main() {
     Deque<int> dq;
 
     // Enqueue some elements
+    cout <<"Enqueue some elements \n";
     dq.enqueue(1);
     dq.enqueue(2);
     dq.enqueue(3);
+    dq.display();  //Expected output: Size = 3, < 0=1 , 1=2 , 2=3 , >
+    cout<<"\n\n";
 
     // Modify elements using []
+    cout <<"Test: Array-like Access and Modification \n";
+    cout << "\nBefore modification \n";
+    dq.display();  //Expected output: Size = 3, < 0=1 , 1=2 , 2=3 , >
+    cout<<"\n\n";
     dq[0] = 10;
     dq[1] = 20;
-
-    // Display elements
-    dq.display(); // Expected output: 10 20 3
+    cout << "After modification\n";
+    dq.display(); // Expected output: Size = 3, < 0=10 , 1=20 , 2=3 , >
 
     return 0;
 }
