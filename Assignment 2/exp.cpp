a48925d6eefe8c6cc4fe05eead5b547c39ddd191
Neel Sadafule <neel.sadafule13@gmail.com>
2024-03-08 10:23:45 -0800

    Updated the experiment program to correctly demonstrate a cache miss

--- Assignment 2/exp.cpp
+++ Assignment 2/exp.cpp
@@ -6,7 +6,13 @@
 using namespace std;
 using namespace std::chrono;
 
-const int num = 100000; // Number of elements to insert
+const int ELEMENT_SIZE = sizeof(int); // Size of an element (integer) in bytes
+// Aiming for a data structure size that significantly exceeds the cache sizes
+// Sum of L1 instruction and data cache sizes in bytes is 196608 (128 KB + 64 KB)
+// Assuming the data size to be at least 10 times this sum to exceed the cache
+const int CACHE_SIZE = 196608; // Total cache size in bytes
+const int DATA_MULTIPLIER = 10; // To ensure data size significantly exceeds cache size
+const int NUM_ELEMENTS = (CACHE_SIZE * DATA_MULTIPLIER) / ELEMENT_SIZE;
 
 int main() {
     OSULL<int> unrolledList;
@@ -14,7 +20,7 @@ int main() {
 
     // Insert elements into the unrolled linked list
     auto start = high_resolution_clock::now();
-    for (int i = 0; i < num; ++i) {
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
         unrolledList.insert(i);
     }
     auto end = high_resolution_clock::now();
@@ -23,16 +29,17 @@ int main() {
 
     // Insert elements into the doubly-linked list
     start = high_resolution_clock::now();
-    for (int i = 0; i < num; ++i) {
-        linkedList.insert(i);
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    linkedList.insert(i);
     }
     end = high_resolution_clock::now();
     auto insertDurationLinked = duration_cast<microseconds>(end - start).count();
     cout << "Doubly-Linked List insertion time: " << insertDurationLinked << " microseconds" << endl;
-
+    
     // Measure traversal time for the unrolled linked list
     start = high_resolution_clock::now();
-    for (int i = 0; i < num; ++i) {
+    // Simulate traversal.
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
         unrolledList.find(i);
     }
     end = high_resolution_clock::now();
@@ -41,7 +48,8 @@ int main() {
 
     // Measure traversal time for the doubly-linked list
     start = high_resolution_clock::now();
-    for (int i = 0; i < num; ++i) {
+    // Simulate traversal.
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
         linkedList.find(i);
     }
     end = high_resolution_clock::now();
